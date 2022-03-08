db6a6987a962a43e32eeb21c285d2ba47a65d279
Sasha Kharina <sashaaa.alexxx@gmail.com>
2022-03-08 12:10:22 +0300

    added map class

--- Map.h
+++ Map.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef MAP
+#define MAP
+
+#include "Platform.h"
+#include <vector>
+
+class Map {
+private:
+	std::vector<Platform*> field;
+	HANDLE* buffer;
+public:
+	Map(HANDLE*);
+	Map(HANDLE*, SHORT);
+	~Map();
+	void init();
+	HANDLE& get_buffer();
+	bool is_platform(COORD, SHORT);
+};
+
+#endif //MAP
